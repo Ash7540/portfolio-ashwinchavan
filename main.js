15c3569686f9cc19a2c2033301a15f7de3170234
Ashwin Chavan <86465817+Ash7540@users.noreply.github.com>
2023-11-26 16:19:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Artificial Intelligence Engineer",
+    "Data Scientist",
+    "Web Developer",
+  ],
+  typeSpeed: 75,
+  backSpeed: 75,
+  backDelay: 1000,
+  loop: true,
+});
